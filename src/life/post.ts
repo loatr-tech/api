d98bacf83e9d4603039758df2386ef8c1df0c761
Shawn Chen <ethanloatrchen@gmail.com>
2021-08-08 18:13:18 -0700

    Polish get posts reponses, add get single post endpoint

--- src/life/post.ts
+++ src/life/post.ts
@@ -1,5 +1,5 @@
 import { Express, Request, Response } from 'express';
-import { FindOptions, MongoClient } from 'mongodb';
+import { FindOptions, MongoClient, ObjectId } from 'mongodb';
 import connect from './connection';
 
 
@@ -14,7 +14,13 @@ export default async function lifeApi(app: Express) {
     // Find documents
     const findOptions = { limit } as FindOptions<any>;
     const postsCursor = postCollections.find({}, findOptions);
-    const posts = await postsCursor.toArray();
+    const posts = (await postsCursor.toArray()).map(post => {
+      return {
+        id: post._id,
+        title: post.title,
+        content: post.content,
+      }
+    });
 
     // Send to the client side
     res.send(
@@ -26,6 +32,21 @@ export default async function lifeApi(app: Express) {
     );
   });
 
+  app.get('/life/post/:postId', async (req: Request, res: Response) => {
+    const { postId } = req.params;
+    const postCollections = client.db('shangan').collection('post');
+    const post: any = await postCollections.findOne({ _id: new ObjectId(postId) });
+
+    // Send to the client side
+    res.send(
+      JSON.stringify({
+        id: post._id,
+        title: post.title,
+        content: post.content,
+      })
+    );
+  });
+
   app.post('/life/post', async (req: Request, res: Response) => {
     const postCollections = client.db('shangan').collection('post');
 
@@ -33,6 +54,9 @@ export default async function lifeApi(app: Express) {
       title: req.body.title,
       content: req.body.content,
       createdAt: new Date(),
+      views: 0,
+      likes: 0,
+      comments: 0,
     };
     const resultsAfterInsert = await postCollections.insertOne(postObject);
     res.send(JSON.stringify(resultsAfterInsert));
